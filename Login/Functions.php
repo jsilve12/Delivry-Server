b502230ffee64b423b5915b0fc2c768612b1c545
Jonathan Silverstein <jsilve@umich.ed>
2018-07-18 17:16:55 -0400

    Added the Verify User function

--- Login/Functions.php
+++ Login/Functions.php
@@ -4,4 +4,22 @@
 		echo json_encode($response);
 		return;
 	}
+	function verify_user()
+	{
+		//Ensures that the user exists
+		if(isset($_POST['email']) && isset($_POST['password'])
+		{
+			$stmt = $pdo->prepare("SELECT * FROM People WHERE email = :em, password = :pa");
+			$stmt->execute(array(
+			    ":em" => $_POST['email'],
+				":pa" => $_POST['password']
+			));
+			
+			if(!empty($stmt->fetchAll(PDO::FETCH_ASSOC)))
+			{
+				return true;
+			}
+		}
+		return false;
+	}
 ?>
