73d666e06f67c319f7e6894b44ddbdf06a4e7521
Jonathan Silverstein <jsilve@umich.ed>
2018-07-18 17:34:56 -0400

    Finished v1 of Address.php

--- Login/Address.php
+++ Login/Address.php
@@ -1,4 +1,31 @@
 <?php
 	require_once("Functions.php");
-	verify_user();
+	
+	//Makes sure the user actually exists
+	$result = array();
+	if(!verify_user())
+	{
+		$result['error'] = "User doesn't exist";
+		done();
+	}
+	
+	//Adds the address to the users SQL entry
+	try(
+	{
+		$stmt = $pdo->prepare("UPDATE People SET address = :adr, longitude = :long, latitude = :lat WHERE email = :em, password = :pa");
+		$stmt->execute(array(
+			":adr" => $_POST['address'],
+			":long" => $_POST['longitude'],
+			":lat" => $_POST['latitude'],
+			":em" => $_POST['email'],
+			":pa" => $_POST['password']
+		));
+		$result['success'] = "Success";
+		done();
+	}
+	catch(e)
+	{
+		$result['error'] = "SQL entered improperly";
+		done();
+	}
 ?>
