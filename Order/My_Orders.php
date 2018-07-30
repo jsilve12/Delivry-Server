af76076d32437a11be6fa23762b43ec773b4df66
Jonathan Silverstein <jsilve@umich.ed>
2018-07-30 19:34:36 -0400

    My_Orders.php done

--- Order/My_Orders.php
+++ Order/My_Orders.php
@@ -1,16 +1,17 @@
 <?php
   require_once("Functions.php");
   $user = start($pdo);
+  echo(implode($user[0], ' '));
   $response = array();
 
   //Yes I know theres a lot of repitiion, and this file will be re-written (JS 7-2018)
   //Orders Placed
   try {
-    $stmt = $pdo->prepare("SELECT p.address, p.addr_description, p.store, i.name, ip.description FROM Order_Placed AS p JOIN Items_Placed AS ip ON p.order_id = ip.order_id JOIN Items AS i ON i.item_id = ip.item_id WHERE p.placed_by = :pb");
+    $stmt = $pdo->prepare("SELECT p.order_id, p.address, p.addr_description, p.store, i.name, ip.description FROM Order_Placed AS p JOIN Items_Placed AS ip ON p.order_id = ip.order_id JOIN Items AS i ON i.item_id = ip.item_id WHERE p.placed_by = :pb");
     $stmt->execute(array(
-      ":pb" => $user['people_id'];
-    ))
-    $response['My Placed Orders'] = $stmt->FetchAll(PDO::FETCH_ASSOC);
+      ":pb" => $user[0]['people_id']
+    ));
+    $response['Placed Orders'] = $stmt->FetchAll(PDO::FETCH_ASSOC);
   } catch (\Exception $e) {
     $response['error'] = "Error with fetching the orders placed";
     done($response);
@@ -18,9 +19,9 @@
 
   //Orders Accepted (Either the placer or deliverer)
   try {
-    $stmt = $pdo->prepare("SELECT p.address, p.addr_description, p.store, i.name, ip.description, p.accepted_by FROM Order_Acccepted AS p JOIN Items_Accepted AS ip ON p.order_id = ip.order_id JOIN Items AS i on i.item_id = ip.item_id WHERE p.placed_by = :pb OR p.accepted_by = :pb")
+    $stmt = $pdo->prepare("SELECT p.order_id, p.address, p.addr_description, p.store, i.name, ip.description, p.accepted_by FROM Order_Accepted AS p JOIN Items_Accepted AS ip ON p.order_id = ip.order_id JOIN Items AS i on i.item_id = ip.item_id WHERE p.placed_by = :pb OR p.accepted_by = :pb");
     $stmt->execute(array(
-      ":pb" =>$user['people_id']
+      ":pb" =>$user[0]['people_id']
     ));
     $response['Accepted Orders'] = $stmt->FetchAll(PDO::FETCH_ASSOC);
   } catch (\Exception $e) {
@@ -29,22 +30,22 @@
 
   //Orders Finished (Either the placer or deliverer)
   try {
-    $stmt = $pdo->prepare("SELECT p.address, p.addr_description, p.store, i.name, ip.description, p.accepted_by, p.price FROM Order_Finished AS p JOIN Items_Finished AS ip ON p.order_id = ip.order_id JOIN Items AS i on i.item_id = ip.item_id WHERE p.placed_by = :pb OR p.accepted_by = :pb")
+    $stmt = $pdo->prepare("SELECT p.order_id, p.address, p.addr_description, p.store, i.name, ip.description, p.accepted_by, p.price FROM Order_Finished AS p JOIN Items_Finished AS ip ON p.order_id = ip.order_id JOIN Items AS i on i.item_id = ip.item_id WHERE p.placed_by = :pb OR p.accepted_by = :pb");
     $stmt->execute(array(
-      ":pb" =>$user['people_id']
+      ":pb" =>$user[0]['people_id']
     ));
     $response['Finished Orders'] = $stmt->FetchAll(PDO::FETCH_ASSOC);
   } catch (\Exception $e) {
-    $response['error'] = "Error with fetching the orders finished"
+    $response['error'] = "Error with fetching the orders finished";
   }
 
   //Conflicts (Either the placer or deliverer)
   try {
-    $stmt = $pdo->prepare("SELECT p.address, p.addr_description, p.store, i.name, ip.description, p.accepted_by,p.price,p.comments FROM Order_Finished AS p JOIN Items_Finished AS ip ON p.order_id = ip.order_id JOIN Items AS i on i.item_id = ip.item_id WHERE p.placed_by = :pb OR p.accepted_by = :pb")
+    $stmt = $pdo->prepare("SELECT p.order_id, p.address, p.addr_description, p.store, i.name, ip.description, p.accepted_by,p.price,p.comments FROM Order_Conflict AS p JOIN Items_Conflict AS ip ON p.order_id = ip.order_id JOIN Items AS i on i.item_id = ip.item_id WHERE p.placed_by = :pb OR p.accepted_by = :pb");
     $stmt->execute(array(
-      ":pb" =>$user['people_id']
+      ":pb" =>$user[0]['people_id']
     ));
-    $response['Finished Orders'] = $stmt->FetchAll(PDO::FETCH_ASSOC);
+    $response['Conflict Orders'] = $stmt->FetchAll(PDO::FETCH_ASSOC);
   } catch (\Exception $e) {
     $response['error'] = "Error with fetching the orders conflicts";
   }
