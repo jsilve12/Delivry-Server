f269d80229cd728f0db6a16fc3018c07638ba364
Jonathan Silverstein <jsilve@umich.ed>
2018-08-01 11:44:45 -0400

    Item.php Done

--- Autocomplete/Item.php
+++ Autocomplete/Item.php
@@ -6,7 +6,7 @@
     //Gets results based on the most common Items
     $stmt = $pdo->prepare("SELECT name FROM Items WHERE name like :it ORDER BY num_called DESC");
     $stmt->execute(array(
-      ":it" => strtolower(trim($_POST['item']))
+      ":it" => "%".strtolower(trim($_POST['item']))."%"
     ));
     $response['result'] = array_slice($stmt->FetchAll(PDO::FETCH_ASSOC), 0, $_POST['num_results']);
     done($response);
