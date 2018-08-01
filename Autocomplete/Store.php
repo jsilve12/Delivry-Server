e843d8429bf396fb46e342b1ed5d7b74f7235680
Jonathan Silverstein <jsilve@umich.ed>
2018-08-01 12:24:29 -0400

    Autocomplete.php Done

--- Autocomplete/Store.php
+++ Autocomplete/Store.php
@@ -1,11 +1,11 @@
 <?php
-  require_once("Store.php");
+  require_once("../Order/Functions.php");
   start($pdo);
 
   try {
     $stmt = $pdo->prepare("SELECT name FROM Stores WHERE name like :na ORDER BY num_called DESC");
     $stmt->execute(array(
-      ":na" => $_POST['name']
+      ":na" => "%".$_POST['name']."%"
     ));
     $response['success'] = array_slice($stmt->FetchAll(PDO::FETCH_ASSOC), 0, $_POST['num_results']);
     done($response);
