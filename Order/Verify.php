3df0eed5a1ff53e2527a194454aff23c160abcf6
Jonathan Silverstein <jsilve@umich.ed>
2018-08-03 12:59:21 -0400

    Verify.php finished v1

---
+++ Order/Verify.php
@@ -0,0 +1,77 @@
+<?php
+  //TODO: Add a portal for the receipt to be submitted
+  require_once("Functions.php");
+  start($pdo);
+
+  //Deletes the entry from Accepted
+  $result = array();
+  try {
+    $stmt = $pdo->prepare("SELECT * FROM Order_Finished WHERE order_id = ".$_POST['order_id']);
+    $stmt->execute();
+    $result = $stmt->FetchAll(PDO::FETCH_ASSOC);
+    if(empty($result))
+    {
+      $response['error'] = "Order not found";
+      done($response);
+    }
+  } catch (\Exception $e) {
+    $response['error'] = "SQL error";
+  }
+  $id;
+
+  //Adds the entry into the new table
+  try {
+    $stmt = $pdo->prepare("INSERT INTO Order_Verified(placed_by, accepted_by, receipt, price, address, addr_description, longitude, latitude, store) VALUES(:pb,:ab,:re,:pr,:ad,:ad_de,:lo,:la,:st)");
+    $stmt->execute(array(
+      ":pb" => $result[0]["placed_by"],
+      ":ab" => $result[0]["accepted_by"],
+      ":re" => $result[0]['receipt'],
+      ":pr" => $result[0]['price'],
+      ":ad" => $result[0]["address"],
+      ":ad_de" => $result[0]["addr_description"],
+      ":lo" => $result[0]["longitude"],
+      ":la" => $result[0]["latitude"],
+      ":st" => $result[0]["store"]
+    ));
+    $id = $pdo->LastInsertId();
+  } catch (\Exception $e) {
+    $response['error'] = "SQL error ";
+    done($response);
+  }
+
+  //Finds the items in the database
+  try{
+    $stmt = $pdo->prepare("SELECT * FROM Items_Verified WHERE order_id = ".$_POST['order_id']);
+    $stmt->execute();
+    $result = $stmt->FetchAll(PDO::FETCH_ASSOC);
+    if(empty($result))
+    {
+      $response['error'] = "SQL error";
+      done($response);
+    }
+  }
+  catch(\Exception $e)
+  {
+    $response['error'] = "SQL error";
+    done($response);
+  }
+    foreach($result as $value)
+    {
+      try {
+        //Enters each item back into the database
+        $stmt = $pdo->prepare("INSERT INTO Items_Verified(order_id, description, item_id, price) VALUES(:oi, :de, :ii, :p)");
+        $stmt->execute(array(
+          ":oi" => $id,
+          ":de" => $value['description'],
+          ":ii" => $value['item_id'],
+          ":p" => $value['price']
+        ));
+      } catch (\Exception $e) {
+        $response['error'] = "SQL error";
+        done($response);
+      }
+    }
+    //Deletes everything
+    $stmt = $pdo->prepare("DELETE FROM Order_Finished WHERE order_id= ".$_POST['order_id']);
+    $stmt->execute();
+?>
