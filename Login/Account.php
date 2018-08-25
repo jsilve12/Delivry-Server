ef29d63c48712c6f52a5868244e92cc862d61a88
Jonathan Silverstein <jsilve@umich.ed>
2018-08-25 18:22:02 -0400

    Modified Account to accept the account info

--- Login/Account.php
+++ Login/Account.php
@@ -1,3 +1,19 @@
 <?php
 	require_once("../Order/Functions.php");
+
+  try
+  {
+  $stmt = $pdo->prepare("UPDATE People SET payment = :ch WHERE email = :em");
+  $stmt->execute(array(
+    ":ch" => $_GET['id'],
+    ":em" => $_GET['code']
+  ));
+  $response['success'] = "Success";
+  done($response);
+}
+catch(\Exception $e)
+{
+  $response['error'] = "SQL entered improperly";
+  done($response);
+}
 ?>
